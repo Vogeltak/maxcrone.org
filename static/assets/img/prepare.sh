53b3e91bebd701c789e3064c04ab46d555c2d8fb
Max Crone <code@maxcrone.org>
2024-07-07 21:04:16 +0200

    Re-transcode webp and add avif for all images

--- static/assets/img/prepare.sh
+++ static/assets/img/prepare.sh
@@ -9,19 +9,25 @@ fi
 photo=$1
 
 echo "Creating 2880 jpg..."
-convert $1 -resize 2880 ${photo/.jpg/_2880.jpg}
+magick $1 -resize 2880 ${photo/.jpg/_2880.jpg}
 
 echo "Creating 1280 jpg..."
-convert $1 -resize 1280 ${photo/.jpg/_1280.jpg}
+magick $1 -resize 1280 ${photo/.jpg/_1280.jpg}
 
 echo "Creating 640 jpg..."
-convert $1 -resize 640 ${photo/.jpg/_640.jpg}
+magick $1 -resize 640 ${photo/.jpg/_640.jpg}
 
 echo "Creating 2880 webp..."
-cwebp ${photo/.jpg/_2880.jpg} -o ${photo/.jpg/_2880.webp}
+magick ${photo/.jpg/_2880.jpg} -quality 50 ${photo/.jpg/_2880.webp}
+echo "Creating 2880 avif..."
+magick ${photo/.jpg/_2880.jpg} ${photo/.jpg/_2880.avif}
 
 echo "Creating 1280 webp..."
-cwebp ${photo/.jpg/_1280.jpg} -o ${photo/.jpg/_1280.webp}
+magick ${photo/.jpg/_1280.jpg} -quality 50 ${photo/.jpg/_1280.webp}
+echo "Creating 1280 avif..."
+magick ${photo/.jpg/_1280.jpg} ${photo/.jpg/_1280.avif}
 
 echo "Creating 640 webp..."
-cwebp ${photo/.jpg/_640.jpg} -o ${photo/.jpg/_640.webp}
+magick ${photo/.jpg/_640.jpg} -quality 50 ${photo/.jpg/_640.webp}
+echo "Creating 1280 avif..."
+magick ${photo/.jpg/_640.jpg} ${photo/.jpg/_640.avif}
--- templates/shortcodes/image.md
+++ templates/shortcodes/image.md
@@ -1,4 +1,5 @@
 <picture>
+    <source srcset="https://f003.backblazeb2.com/file/maxcrone-org/img/{{ name }}_{% if size %}{{ size }}{% else %}1280{% endif %}.avif" type="image/avif">
     <source srcset="https://f003.backblazeb2.com/file/maxcrone-org/img/{{ name }}_{% if size %}{{ size }}{% else %}1280{% endif %}.webp" type="image/webp">
     <img src="https://f003.backblazeb2.com/file/maxcrone-org/img/{{ name }}_{% if size %}{{ size }}{% else %}1280{% endif %}.jpg" {% if caption %}alt="{{ caption }}"{% endif %} loading="lazy">
 </picture>
