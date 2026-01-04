2fa430b15433cc5591fa0e40b39bbad64aefa780
Max Crone <code@maxcrone.org>
2026-01-05 00:51:34 +0100

    Fix b2 file upload command

--- static/assets/img/publish.sh
+++ static/assets/img/publish.sh
@@ -12,5 +12,5 @@ prefix=$1
 
 for file in $(find . -name "$prefix*" ); do
     echo "Uploading ${file:2}";
-    b2 upload_file maxcrone-org $file img/${file:2};
+    b2 file upload maxcrone-org $file img/${file:2};
 done
